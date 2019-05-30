bb23a4fcf1537643081345a02cc86caee51dc94c
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-05-29 17:14:44 -0700

    Edded the rest of endpoints for MVP

--- cohorts/cohortsRouter.js
+++ cohorts/cohortsRouter.js
@@ -48,4 +48,84 @@ router.get('/:id', async (req, res) => {
     }
 });
 
+
+// GET all students for the cohort with specified id
+// for route /api/cohorts/:id/students
+// returns a list of all the students for the particular cohort in the database (or an empty array if there's no students)
+router.get('/:id/students', async (req, res) => {
+    try {
+        const students = await db('students')
+            .where('cohorts_id', req.params.id);
+        res.status(200).json(students);
+      } catch (error) {
+        res.status(500).json({
+          message: 'Error retrieving students',
+          error: error
+        });
+      }
+})
+
+// POST for /api/cohorts
+// Returns new cohort object
+router.post('/', (req, res) => {
+    db('cohorts')
+        .insert(req.body, 'id')
+        .then(ids => {
+            db('cohorts')
+                .where({ id: ids[0] })
+                .first()
+                .then(cohort => {
+                    res.status(201).json(cohort);
+                })
+                .catch(error => {
+                    res.status(500).json({
+                        message: 'Error adding cohort',
+                        error: error
+                    });
+                });
+        })
+        .catch(error => {
+            res.status(500).json({
+                message: 'Error adding cohort',
+                error: error
+              });
+        });
+});
+
+// DELETE for /api/cohorts/:id
+// Returns number of records has been deleted
+router.delete('/:id', (req, res) => {
+    db('cohorts')
+        .where({ id: req.params.id })
+        .del()
+        .then(count => {
+            if(count > 0) {
+                res.status(200).json(count);
+            } else {
+                res.status(404).json({ message: 'Cohort with specified id doesn\'t exist'});
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ message: 'Error deleting cohort', error: error });
+        });
+});
+
+// PUT for /api/cohorts/:id
+// Returns number of records has been updated
+router.put('/:id', (req, res) => {
+    db('cohorts')
+        .where({ id: req.params.id })
+        .update(req.body)
+        .then(count => {
+            if(count > 0) {
+                res.status(200).json(count);
+            } else {
+                res.status(404).json({ message: 'Cohort with specified id doesn\'t exist'});
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ message: 'Error updating cohort', error: error });
+        });
+});
+
 module.exports = router;
